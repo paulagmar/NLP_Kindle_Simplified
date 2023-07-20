73310617d4713d077e0bb43c1bc5e4b563bd7e74
Paula Guijarro <47014501+paulagmar@users.noreply.github.com>
2023-07-20 12:16:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+  # Load the data.
+  data = pd.read_csv('data.csv')
+
+  # Preprocess the text.
+  data['review_processed'] = data['review'].apply(preprocess_text)
+
+  # Generate a histogram of the distribution of words in the review text.
+  generate_histogram(data, 'review_processed')
+
+  # Calculate the frequency of words in the review text.
+  word_frequencies = calculate_word_frequency(data, 'review_processed')
+
+  # Print the top 10 most frequent words.
+  print(word_frequencies[:10])
